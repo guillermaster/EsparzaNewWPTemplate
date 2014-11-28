ba697fd94ad32f28b448dfe5af07c8665bd52c76
Guillermo Pincay <guillermaster@gmail.com>
2014-11-27 22:34:15 -0500

    Web parts post type

--- wordpress/Esparza-Theme/functions.php
+++ wordpress/Esparza-Theme/functions.php
@@ -40,4 +40,57 @@
     return $classes;
     }
 
+
+    // Our custom post type function
+    function create_posttype() {
+
+    register_post_type( 'movies',
+        // CPT Options
+        array('labels' => array(
+                            'name' => __( 'Web Parts' ),
+                            'singular_name' => __( 'Web Part' )
+                        ),
+                'public' => true,
+                'has_archive' => false,
+                'rewrite' => array('slug' => 'webparts'),
+                'taxonomies' => array( 'webpartarea' ),
+
+            )
+        );
+    }
+    // Hooking up our function to theme setup
+    add_action( 'init', 'create_posttype' );
+
+
+    // create two taxonomies, genres and writers for the post type "book"
+    function create_book_taxonomies() {
+        // Add new taxonomy, NOT hierarchical (like tags)
+        $labels = array(
+            'name'                       => _x( 'Web Part Areas', 'taxonomy general name' ),
+            'singular_name'              => _x( 'Web Part Area', 'taxonomy singular name' ),
+            'search_items'               => __( 'Search Web Part Areas' ),
+            'all_items'                  => __( 'All Web Part Areas' ),
+            'parent_item'                => __( 'Parent Web Part Area' ),
+            'parent_item_colon'          => __( 'Parent Web Part Area:' ),
+            'edit_item'                  => __( 'Edit Web Part Area' ),
+            'update_item'                => __( 'Update Web Part Area' ),
+            'add_new_item'               => __( 'Add New Web Part Area' ),
+            'new_item_name'              => __( 'New Web Part Area Name' ),
+            'menu_name'                  => __( 'Web Part Areas' ),
+        );
+
+        $args = array(
+            'hierarchical'          => true,
+            'labels'                => $labels,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+            'rewrite'               => array( 'slug' => 'webpart_areas' ),
+        );
+
+        register_taxonomy( 'webpartarea', array('webparts'), $args );
+    }
+    // hook into the init action and call create_book_taxonomies when it fires
+    add_action( 'init', 'create_book_taxonomies', 0 );
+
 ?>
